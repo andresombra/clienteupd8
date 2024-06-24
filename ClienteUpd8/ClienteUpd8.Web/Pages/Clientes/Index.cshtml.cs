d43a20aa2dd591da1fab62775d07579c28e5c24f
andresombra <andresombra.net@gmail.com>
2024-06-24 19:10:26 -0300

    Refatoracao

--- ClienteUpd8/ClienteUpd8.Web/Pages/Clientes/Index.cshtml.cs
+++ ClienteUpd8/ClienteUpd8.Web/Pages/Clientes/Index.cshtml.cs
@@ -19,6 +19,46 @@ namespace ClienteUpd8.Web.Pages.Clientes
 
         public async Task OnGet()
         {
+            var cli1 = new Cliente()
+            {
+                
+                Nome = "Andre Sombra"
+                ,
+                DataNascimento = new DateTime(1975, 3, 31)
+                ,
+                Cidade = "Fortaleza"
+                ,
+                Estado = "Ceara"
+                ,
+                Endereco = "Rua Nove, 40"
+                ,
+                CPF = "580.941.213-00"
+                ,
+                Sexo = "M"
+            };
+                await _clienteService.Incluir(cli1);
+
+            var cli2 = new Cliente()
+            {
+
+                Nome = "Andre Sombra XXXXX"
+                ,
+                DataNascimento = new DateTime(1975, 3, 31)
+                ,
+                Cidade = "Fortaleza"
+                ,
+                Estado = "Ceara"
+                ,
+                Endereco = "Rua Nove, 40"
+                ,
+                CPF = "580.941.213-00"
+                ,
+                Sexo = "M"
+            };
+            await _clienteService.Incluir(cli2);
+
+
+
             Cliente = await _clienteService.ListarTodosClientes();
         }
     }
