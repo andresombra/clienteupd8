ed4abc3b3172e375ad1e113e570de34e7c03b1a6
andresombra <andresombra.net@gmail.com>
2024-06-24 11:30:18 -0300

    Implementado IClienteService

--- ClienteUpd8/ClienteUpd7.Application/Interfaces/IClienteService.cs
+++ ClienteUpd8/ClienteUpd7.Application/Interfaces/IClienteService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClienteUpd8.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace ClienteUpd7.Application.Interfaces
 {
     public interface IClienteService
     {
+        Task<IEnumerable<Cliente>> ListarTodosClientes();
+        Cliente BuscarId(int id);
+        Task Incluir(Cliente cliente);
+        Task Alterar(Cliente cliente);
+        void Excluir(int id);
     }
 }
--- ClienteUpd8/ClienteUpd7.Application/Services/ClienteService.cs
+++ ClienteUpd8/ClienteUpd7.Application/Services/ClienteService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClienteUpd7.Application.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
