b1fe9f63cabbe9b9cd17317793bdf71394dba9bd
andresombra <andresombra.net@gmail.com>
2024-06-24 11:40:45 -0300

    Implementado ClienteService

--- ClienteUpd8/ClienteUpd7.Application/Services/ClienteService.cs
+++ ClienteUpd8/ClienteUpd7.Application/Services/ClienteService.cs
@@ -1,4 +1,6 @@
 ﻿using ClienteUpd7.Application.Interfaces;
+using ClienteUpd8.Domain.Entities;
+using ClienteUpd8.Domain.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,35 @@ namespace ClienteUpd7.Application.Services
 {
     public class ClienteService : IClienteService
     {
+        private readonly IClienteRepository _clienteRepository;
+        public ClienteService(IClienteRepository clienteRepository)
+        {
+          _clienteRepository = clienteRepository;
+        }
+
+        public async Task Alterar(Cliente cliente)
+        {
+            await _clienteRepository.Update(cliente);
+        }
+
+        public async Task<Cliente> BuscarId(int id)
+        {
+            return _clienteRepository.GetById(id); 
+        }
+
+        public void Excluir(int id)
+        {
+            _clienteRepository.Delete(id);
+        }
+
+        public Task Incluir(Cliente cliente)
+        {
+            return _clienteRepository.Add(cliente);
+        }
+
+        public async Task<IEnumerable<Cliente>> ListarTodosClientes()
+        {
+            return await _clienteRepository.GetAll();
+        }
     }
 }
